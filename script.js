978bb2ba2d94e9d6263c0deba829040b754a07ab
“AdeleAps” <adelea@inbox.lv>
2022-04-10 18:36:40 +0300

    Finished basic CSS for reservations

--- script.js
+++ script.js
@@ -34,24 +34,66 @@ $("#social-btn").click(function() {
 // form
 
 $("#open-form").click(function() {
-  $("#form-section").toggle();
+  $("#form-section").toggle("medium");
+  $("#form-section").css("display", "flex");
+  $("#open-form").toggleClass("btn-active");
 });
 
 (function() {
-  'use strict';
-  window.addEventListener('load', function() {
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.getElementsByClassName('needs-validation');
-    // Loop over them and prevent submission
-    var validation = Array.prototype.filter.call(forms, function(form) {
-      form.addEventListener('submit', function(event) {
-        if (form.checkValidity() === false) {
-          event.preventDefault();
-          event.stopPropagation();
-        }
-        form.classList.add('was-validated');
-      }, false);
-    });
-  }, false);
+'use strict';
+window.addEventListener('load', function() {
+  let forms = document.getElementsByClassName('needs-validation');
+  let validation = Array.prototype.filter.call(forms, function(form) {
+    form.addEventListener('submit', function(event) {
+      if (form.checkValidity() === false) {
+        event.preventDefault();
+        event.stopPropagation();
+      }
+      form.classList.add('was-validated');
+    }, false);
+  });
+}, false);
 })();
+
+// person selector in the form
+
+let substract = function() {
+let input = $("#person-input").text();
+let number = parseInt(input.charAt(0));
+let newNumber = number - 1;
+
+$('#plus').css("pointerEvents", "auto");
+$("#person-input").text(newNumber + " people");
+if (newNumber == 1) {
+  $("#person-input").text(newNumber + " person");
+  $('#minus').css("pointerEvents", "none");
+}
+}
+
+let add = function() {
+let input = $("#person-input").text();
+let number = parseInt(input.charAt(0));
+let newNumber = number + 1;
+
+$('#minus').css("pointerEvents", "auto");
+$("#person-input").text(newNumber + " people");
+if (newNumber == 1) {
+  $("#person-input").text(newNumber + " person")
+}
+else if (newNumber == 8) {
+  $('#plus').css("pointerEvents", "none");
+}
+}
+
+$("#plus").click(add);
+$("#minus").click(substract);
+
+
+// $("#choice").change(function () {
+//   if($(this).val() == "0") $(this).addClass("empty");
+//   else $(this).removeClass("empty")
+// });
+
+// $("#choice").change();
+
 
